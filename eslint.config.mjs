f915d6834cfa76ea86f0b76252e0b63a36ad9c8e
Antony74 <akb@akb.me.uk>
2025-06-02 19:41:19 +0100

    Correct the build and lint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import noMixedOperators from 'eslint-plugin-no-mixed-operators';
@@ -9,10 +8,6 @@ export default defineConfig([
         files: ['**/*.{js,mjs,cjs,ts}'],
         plugins: { js, noMixedOperators },
         extends: ['js/recommended'],
-    },
-    {
-        files: ['**/*.{js,mjs,cjs,ts}'],
-        languageOptions: { globals: globals.browser },
     },
     tseslint.configs.recommended,
     {
