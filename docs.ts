fbdf1d1d3129af43a22410f65aa19cddead37246
Antony74 <akb@akb.me.uk>
2025-07-06 20:14:46 +0100

    docs script

--- docs.ts
+++ docs.ts
@@ -2,11 +2,13 @@ import fsp from 'fs/promises';
 import path from 'path';
 
 import { rimraf } from 'rimraf';
+import * as typedoc from 'typedoc';
 
 const main = async () => {
     const docsPath = path.join(__dirname, 'docs');
 
     console.log('clean up');
+
     const filesInDocs = await fsp.readdir(docsPath);
 
     await Promise.all(
@@ -15,8 +17,22 @@ const main = async () => {
                 (filename) =>
                     filename !== 'header.md' && filename !== 'footer.md',
             )
-            .map((filename) => rimraf(path.join(docsPath, filename))),
+            .map((filename) => path.join(docsPath, filename))
+            .map((pathname) => rimraf(pathname)),
     );
+
+    console.log('run typedoc');
+
+    const app = await typedoc.Application.bootstrap({
+        entryPoints: ['src/*']
+    });
+
+    const project = await app.convert();
+
+    if (project) {
+        await app.generateDocs(project, docsPath);
+        await app.generateJson(project, path.join(docsPath, 'docs.json'));
+    }
 
     console.log('done');
 };
