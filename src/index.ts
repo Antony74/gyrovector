91a3fe2f2d459bb14aeaf5c9dd9a3262dd29e55d
Antony74 <akb@akb.me.uk>
2025-07-13 20:37:58 +0100

    More exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+export * from './gyrovector';
 export * from './gyrovectorSpaceFactory';
+export * from './gyrovectorXY';
+export * from './vector';
+export * from './vectorLike';
+export * from './vectorSpaceLike';
+export * from './vectorXY';
