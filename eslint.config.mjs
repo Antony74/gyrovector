4c5418b899983ec51f61dd38a7fac094ad868fc1
Antony74 <akb@akb.me.uk>
2025-08-16 13:36:45 +0100

    eslint-plugin-functional, and switch all rules on so I can see what I'm getting rid of

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,13 @@ import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import { defineConfig } from 'eslint/config';
 import noMixedOperators from 'eslint-plugin-no-mixed-operators';
+import functional from 'eslint-plugin-functional';
 
 export default defineConfig([
     {
         files: ['**/*.{js,mjs,cjs,ts}'],
-        plugins: { js, noMixedOperators },
-        extends: ['js/recommended'],
+        plugins: { js, noMixedOperators, functional },
+        extends: ['js/recommended', 'functional/strict'],
     },
     tseslint.configs.recommended,
     {
@@ -25,6 +26,31 @@ export default defineConfig([
                 },
             ],
             'noMixedOperators/no-mixed-operators': ['error'],
+            'no-param-reassign': ['error'],
+            'functional/functional-parameters': ['error'],
+            'functional/no-promise-reject': ['error'],
+            'functional/no-throw-statements': ['error'],
+            'functional/no-try-statements': ['error'],
+            'functional/immutable-data': ['error'],
+            'functional/no-let': ['error'],
+            'functional/prefer-immutable-types': ['error'],
+            'functional/prefer-readonly-type': ['error'],
+            'functional/type-declaration-immutability': ['error'],
+            'functional/no-class-inheritance': ['error'],
+            'functional/no-classes': ['error'],
+            'functional/no-mixed-types': ['error'],
+            'functional/no-this-expressions': ['error'],
+            'functional/no-conditional-statements': ['error'],
+            'functional/no-expression-statements': ['error'],
+            'functional/no-loop-statements': ['error'],
+            'functional/no-return-void': ['error'],
+            'functional/prefer-property-signatures': ['error'],
+            'functional/prefer-tacit': ['error'],
+            'functional/readonly-type': ['error'],
+        },
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: { projectService: true },
         },
     },
 ]);
